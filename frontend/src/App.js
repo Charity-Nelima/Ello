ce2f46f2654523cd844d3e0172a13ec75cb6efbc
Charity-Nelima <wa.nelimacharity@gmail.com>
2024-06-09 11:21:04 +0300

    code review

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,3 @@
-// import './App.css';
-// import SearchBar from './components/searchBar';
-// import Cardd from './components/Display';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <SearchBar/>
-//       <Cardd/>
-
-//     </div>
-//   );
-// }
-
-// export default App;
-// App.js or index.js
 import './App.css';
 import SearchBar from './components/searchBar';
 import Cardd from './components/Display';import { ThemeProvider, createTheme } from '@mui/material/styles';
