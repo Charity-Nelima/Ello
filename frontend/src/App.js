674430e2ceb43abc51cb7a857309701761465a07
Charity-Nelima <wa.nelimacharity@gmail.com>
2024-06-09 09:54:45 +0300

    add react setup of the frontend and material ui installation

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from './components/searchBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar/>
+
+    </div>
+  );
+}
+
+export default App;
